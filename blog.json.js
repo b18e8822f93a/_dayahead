91a7e0589447f38802bc08cc9c445df939fe4a2f
daniel <daniel>
2025-05-05 01:35:03 +0100

    _

---
+++ blog.json.js
@@ -0,0 +1,28 @@
+const tempBlog = [
+    {
+        "title": "Welcome to the Day Ahead game",
+        "txt": `
+       The aim of this game is to forecast the daily price of power in the UK, this is known in the business as the Dayahead price, this is no trivial task and you'll be taking the role of a power trader. 
+  </br></br>
+How this price is set can be complicated, so to begin simply we'll introduce the major inputs need to be considered; the forecasted demand, that is how much power is expected to be used by houses and industries, the forecasted solar and wind generation and finally the cost of relavant fuels used in power stations, in the case of the UK this will be Gas.
+  </br></br>
+Through these blogs posts we'll develop these ideas, more inputs that feed into price formation, the datasets available, details about the UK power grid and markets, the importance of meterology and the interesting techniques applied to make such forecasts seen in industry.
+  </br></br>
+The Dayahead price is set once a day through an auction, so we think this games lends itself quite well to the Wordle game play model, wherein brief daily visits are common and over time allow you to really get familar the dynamics of the market and see how your performance improves.
+  </br></br>
+Finally why is there a dayahead price, well the job of the national grid is to ensure that all industries and homes have the power they need when they need it, and due to the unique nature of electricity in that it cannot be stored, at least in sufficiently large quantities, it needs to be generated, transmitted down wires, distributed to the regions of the UK and finally consumed by us and industry, all in real time!
+  </br></br>
+The dayahead auction determines the price of the electricity for the next delivery day, so if today was Monday the auction would be undertaken at 11:30am today for the delivery day, tomorrow, 'Tuesday', hence the term day ahead. As part of forming this price the grid determines which generators will provide the electricity and which suppliers will buy that electricity and therefore meet the demands of the country.
+
+
+
+        </br></br>
+        Please feel free to make your first forecast today, no registration or emails or passwords required !
+        `,
+        "date": "April 1st 2025",
+        "tag1": "GAME",
+        "tag2": "UK",
+    },
+ 
+    
+]
